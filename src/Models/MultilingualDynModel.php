2b5460397b153ceebb2f316785e245e5f177d270
Alexander Naumov <mail@alexandernaumov.de>
2020-06-04 10:22:59 +0200

    fix find by alias handler

--- src/Models/MultilingualDynModel.php
+++ src/Models/MultilingualDynModel.php
@@ -26,30 +26,19 @@ class MultilingualDynModel extends Multilingual {
         parent::__construct( $objResult );
     }
 
-    public static function findByIdOrAlias( $varId, array $arrOptions = [] ) {
+    public static function findByIdOrAlias($varId, array $arrOptions=[]) {
 
-        $blnIsAlias = !preg_match( '/^[1-9]\d*$/', $varId );
-
-        if ( !$blnIsAlias && empty( $arrOptions ) ) {
-
-            $objModel = Registry::getInstance()->fetch( static::$strTable, $varId );
-
-            if ($objModel !== null) {
-
-                return $objModel;
-            }
+        if (!isset($arrOptions['column']) && !is_array($arrOptions['column'])) {
+            $arrOptions['column'] = [];
         }
+        if (!isset($arrOptions['value']) && !is_array($arrOptions['value'])) {
+            $arrOptions['value'] = [];
+        }
+        $arrOptions['column'][] = !preg_match('/^[1-9]\d*$/', $varId) ? "t1.alias=?" : "t1.id=?";
+        $arrOptions['value'][] = $varId;
+        $arrOptions['limit'] = 1;
+        $arrOptions['return'] = 'Model';
 
-        $arrOptions = array_merge(
-            [
-                'limit'  => 1,
-                'column' => $blnIsAlias ? [ "t1.alias=?" ] : [ "t1.id=?" ],
-                'value'  => $varId,
-                'return' => 'Model'
-            ],
-            $arrOptions
-        );
-
-        return static::find( $arrOptions );
+        return static::find($arrOptions);
     }
 }
