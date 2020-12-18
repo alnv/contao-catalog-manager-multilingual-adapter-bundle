8c317cf183fae094dd770a462e2b4c8346d34c76
Alexander Naumov <mail@alexandernaumov.de>
2020-12-18 09:03:45 +0100

    tidy up code

--- src/Hooks/CatalogWizard.php
+++ src/Hooks/CatalogWizard.php
@@ -4,18 +4,14 @@ namespace Alnv\ContaoCatalogManagerMultilingualAdapterBundle\Hooks;
 
 class CatalogWizard {
 
-    public function parseCatalogField( $arrField, $arrCatalogField ) {
+    public function parseCatalogField($arrField, $arrCatalogField) {
 
-        if ( $arrCatalogField['translatableFor'] ) {
-
-            $arrLanguages = \StringUtil::deserialize( $arrCatalogField['translatableFor'], true );
-
-            if ( in_array( '*', $arrLanguages ) ) {
-
+        if ($arrCatalogField['translatableFor']) {
+            $arrLanguages = \StringUtil::deserialize($arrCatalogField['translatableFor'], true);
+            if (in_array('*', $arrLanguages)) {
                 $arrField['eval']['translatableFor'] = '*';
             }
             else {
-
                 $arrField['eval']['translatableFor'] = $arrLanguages;
             }
         }
@@ -23,12 +19,11 @@ class CatalogWizard {
         return $arrField;
     }
 
-    public function parseCatalog( $arrCatalog ) {
+    public function parseCatalog($arrCatalog) {
 
-        if ( $arrCatalog['dataContainer'] == 'Multilingual' ) {
-
+        if ($arrCatalog['dataContainer'] == 'Multilingual') {
             $arrCatalog['_table'] = 't1';
-            $GLOBALS['CM_MODELS'][ $arrCatalog['table'] ] = 'Alnv\ContaoCatalogManagerMultilingualAdapterBundle\Models\MultilingualDynModel';
+            $GLOBALS['CM_MODELS'][$arrCatalog['table']] = 'Alnv\ContaoCatalogManagerMultilingualAdapterBundle\Models\MultilingualDynModel';
         }
 
         return $arrCatalog;
